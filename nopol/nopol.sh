ead1e754ed00a17e8f31c18a3ed98c9f9682126d
Favio D. DeMarco <favio.demarco@inria.fr>
2013-09-24 16:41:51 +0200

    heap dump on outofmemoryerror

--- nopol/nopol.sh
+++ nopol/nopol.sh
@@ -6,10 +6,9 @@ CLASSPATH=$DIR/lib/com.gzoltar-0.0.3-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$DIR/src/main/resources
 CLASSPATH=$CLASSPATH:$DIR/target/nopol-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 MAINCLASS="fr.inria.lille.nopol.Main"
-JAVA_FLAGS="-Xms2g -Xmx8g -XX:PermSize=128M"
+JAVA_FLAGS="-Xms2g -Xmx16g -XX:PermSize=128M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap_dump.hprof"
 
 COMMAND="java $JAVA_FLAGS -cp $CLASSPATH $MAINCLASS $*"
 
-echo "Running: $COMMAND"
-
+echo "Running: $COMMAND \n"
 $COMMAND
