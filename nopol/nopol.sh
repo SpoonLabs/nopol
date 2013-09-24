fd3caa262fb4ac28c04fd25ad1ba5342e4c434f0
Favio D. DeMarco <favio.demarco@inria.fr>
2013-09-24 16:45:31 +0200

    upper bound

--- nopol/nopol.sh
+++ nopol/nopol.sh
@@ -6,7 +6,7 @@ CLASSPATH=$DIR/lib/com.gzoltar-0.0.3-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$DIR/src/main/resources
 CLASSPATH=$CLASSPATH:$DIR/target/nopol-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 MAINCLASS="fr.inria.lille.nopol.Main"
-JAVA_FLAGS="-Xms2g -Xmx16g -XX:PermSize=128M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap_dump.hprof"
+JAVA_FLAGS="-Xms2g -Xmx16g -XX:PermSize=128M -XX:MaxPermSize=1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap_dump.hprof"
 
 COMMAND="java $JAVA_FLAGS -cp $CLASSPATH $MAINCLASS $*"
 
