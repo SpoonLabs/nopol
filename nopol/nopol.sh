240081907d8d6923dae3011ffd8a92b0219d8a7e
Favio D. DeMarco <favio.demarco@inria.fr>
2013-09-24 11:19:03 +0200

    Memory and GC flags

--- nopol/nopol.sh
+++ nopol/nopol.sh
@@ -5,9 +5,10 @@ DIR="`dirname $0`"
 CLASSPATH=$DIR/lib/com.gzoltar-0.0.3-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$DIR/src/main/resources
 CLASSPATH=$CLASSPATH:$DIR/target/nopol-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-MAINCLASS=fr.inria.lille.nopol.Main
+MAINCLASS="fr.inria.lille.nopol.Main"
+JAVA_FLAGS="-Xms2g -Xmx8g -XX:PermSize=128M"
 
-COMMAND="java -cp $CLASSPATH $MAINCLASS $*"
+COMMAND="java $JAVA_FLAGS -cp $CLASSPATH $MAINCLASS $*"
 
 echo "Running: $COMMAND"
 
