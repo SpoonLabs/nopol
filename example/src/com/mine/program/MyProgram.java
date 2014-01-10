969fc722078cd4a4edeaeda099e5d44a71bf9e9e
Jifeng <jifeng@ubuntu.ubuntu-domain>
2014-01-10 13:45:50 +0100

    example

---
+++ example/src/com/mine/program/MyProgram.java
@@ -0,0 +1,17 @@
+package com.mine.program;
+
+public class MyProgram {
+
+	public int abs(int a, int b)
+	{
+		if(a > b)
+		//if(a < b)
+		{
+			return b - a;
+		}
+		
+		
+		return a - b;
+	}
+	
+}
