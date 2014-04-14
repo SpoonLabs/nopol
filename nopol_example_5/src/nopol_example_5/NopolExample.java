4906078838330bdd14fd54dc90502bc4fb6c6734
Maxime Clement <maxime.clement@inria.fr>
2014-04-14 11:44:25 +0200

    Easy example fixable by Nopol, Custom implementation of negate(int a)

---
+++ nopol_example_5/src/nopol_example_5/NopolExample.java
@@ -0,0 +1,19 @@
+package nopol_example_5;
+
+import java.util.Arrays;
+
+public class NopolExample {
+
+	/*
+	 * Return -a if a isn't negative number, otherwise return a if it's already negative number
+	 */
+	public int negate(int a){
+		int r = 1;		
+		
+		// FIX : precondition missing : if ( -1<a  )
+		r = -1;				
+		return r*a;		
+	}
+	
+	
+}
