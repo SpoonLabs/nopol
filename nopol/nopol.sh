a27acc5109d3981600a4109bb0d1b608854ec66c
Maxime Clement <maxime.clement@inria.fr>
2014-04-16 11:01:39 +0200

    Adding executed time at the end of the debug file Can add lib folders to the project you want to fix

--- nopol/nopol.sh
+++ nopol/nopol.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+START=$(date +%s)
 DIR="`dirname $0`"
+LIB="$3"
 
 CLASSPATH=$DIR/lib/com.gzoltar-0.0.3-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$DIR/src/main/resources
 CLASSPATH=$CLASSPATH:$DIR/target/nopol-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+CLASSPATH=$CLASSPATH:$LIB
 MAINCLASS="fr.inria.lille.nopol.Main"
-JAVA_FLAGS="-Xms2g -Xmx16g -XX:PermSize=128M -XX:MaxPermSize=1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap_dump.hprof"
+JAVA_FLAGS="-Xms512m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap_dump.hprof"
 
-COMMAND="java $JAVA_FLAGS -cp $CLASSPATH $MAINCLASS $*"
-
+COMMAND="java $JAVA_FLAGS -cp $CLASSPATH $MAINCLASS $1 $2"
 echo "Running: $COMMAND \n"
 $COMMAND
+
+END=$(date +%s)
+EXECTIME=$(($END - $START))
+echo "Time : $EXECTIME seconds" >> debug.txt
+
