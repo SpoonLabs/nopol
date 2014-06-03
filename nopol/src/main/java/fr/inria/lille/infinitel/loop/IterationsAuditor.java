79526ab98dad43db34eba01aefc133666bb4a0c8
srlm <cobyto@gmail.com>
2014-06-03 02:45:22 +0200

    finished IterationsAuditor; can be used to detect infinite-loops

--- nopol/src/main/java/fr/inria/lille/infinitel/loop/IterationsAuditor.java
+++ nopol/src/main/java/fr/inria/lille/infinitel/loop/IterationsAuditor.java
@@ -1,12 +1,14 @@
 package fr.inria.lille.infinitel.loop;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.code.CtWhile;
+import fr.inria.lille.commons.collections.ListLibrary;
 import fr.inria.lille.commons.collections.MapLibrary;
 import fr.inria.lille.commons.collections.SetLibrary;
 import fr.inria.lille.commons.spoon.SpoonLibrary;
@@ -14,29 +16,36 @@ import fr.inria.lille.commons.spoon.SpoonLibrary;
 public class IterationsAuditor {
 
 	public static void attachTo(CtWhile loopStatement, int loopID) {
-		CtExpression<Boolean> modifiedLoopingExpression = iterationTrackingCondition(loopStatement);
-		CtStatement loopTerminationHint = loopTerminationHint(loopStatement, loopID);
+		String variableName = counterVariableName(loopID);
+		CtStatement beforeStatement = beforeLoopStatement(loopStatement, variableName);
+		CtExpression<Boolean> modifiedLoopingExpression = iterationTrackingCondition(loopStatement, variableName);
+		CtStatement afterStatement = afterLoopStatement(loopStatement, variableName, loopID);
+		loopStatement.insertBefore(beforeStatement);
 		loopStatement.setLoopingExpression(modifiedLoopingExpression);
-		loopStatement.insertAfter(loopTerminationHint);
+		loopStatement.insertAfter(afterStatement);
 	}
 	
-	private static CtExpression<Boolean> iterationTrackingCondition(CtWhile loopStatement) {
-		String codeSnippet = canonicalName() + ".signalConditionEvaluation()";
-		return SpoonLibrary.composedExpression(codeSnippet, BinaryOperatorKind.AND, loopStatement.getLoopingExpression());
+	private static String counterVariableName(int loopID) {
+		return String.format("loopIterations_%d", loopID);
 	}
-
-	private static CtStatement loopTerminationHint(CtWhile loopStatement, int loopID) {
-		String codeSnippet = canonicalName() + String.format(".signalLoopEnd(%d)", loopID);
+	
+	private static CtStatement beforeLoopStatement(CtWhile loopStatement, String variableName) {
+		String codeSnippet = String.format("int %s = 0", variableName);
 		return SpoonLibrary.statementFrom(codeSnippet, loopStatement.getParent());
 	}
 	
-	public static boolean signalConditionEvaluation() {
-		return instance().recordNewIteration();
+	private static CtExpression<Boolean> iterationTrackingCondition(CtWhile loopStatement, String variableName) {
+		String codeSnippet = canonicalName() + String.format(".instance().threshold() > (++%s)", variableName);
+		return SpoonLibrary.composedExpression(codeSnippet, BinaryOperatorKind.AND, loopStatement.getLoopingExpression());
+	}
+
+	private static CtStatement afterLoopStatement(CtWhile loopStatement, String variableName, int loopID) {
+		String codeSnippet = canonicalName() + String.format(".signalLoopEnd(%s, %d)", variableName, loopID);
+		return SpoonLibrary.statementFrom(codeSnippet, loopStatement.getParent());
 	}
 	
-	public static void signalLoopEnd(int loopID) {
-		instance().updateMaximumIterationsOf(loopID);
-		instance().resetRecordedIterations();
+	public static void signalLoopEnd(int iterations, int loopID) {
+		instance().addRecordOf(iterations, loopID);
 	}
 	
 	public static Collection<Integer> infiniteLoopIDs() {
@@ -47,27 +56,26 @@ public class IterationsAuditor {
 		return instance().getClass().getCanonicalName();
 	}
 	
-	private static IterationsAuditor instance() {
+	public static IterationsAuditor instance() {
 		if (instance == null) {
-			instance = new IterationsAuditor(1E6);
+			instance = new IterationsAuditor();
 		}
 		return instance;
 	}
 	
-	private IterationsAuditor(Number iterationsLimit) {
-		threshold = iterationsLimit.intValue();
-		maximumIterations = MapLibrary.newHashMap();
-		resetRecordedIterations();
+	private IterationsAuditor() {
+		loopIterationsRecord = MapLibrary.newHashMap();
+		resetThreshold();
 	}
 	
-	private void updateMaximumIterationsOf(int loopID) {
-		int oldValue = MapLibrary.getPutIfAbsent(maximumIterations(), loopID, 0);
-		maximumIterations().put(loopID, Math.max(recordedIterations(), oldValue));
+	private void addRecordOf(int iterations, int loopID) {
+		List<Integer> loopRecord = MapLibrary.getPutIfAbsent(iterationsRecord(), loopID, (List) ListLibrary.newArrayList());
+		loopRecord.add(iterations);
 	}
 	
 	private Collection<Integer> loopIDsOverThreshold() {
 		Collection<Integer> ids = SetLibrary.newHashSet();
-		for (Integer loopID : maximumIterations().keySet()) {
+		for (Integer loopID : iterationsRecord().keySet()) {
 			if (reachesThreshold(loopID)) {
 				ids.add(loopID);
 			}
@@ -76,37 +84,27 @@ public class IterationsAuditor {
 	}
 	
 	private boolean reachesThreshold(Integer loopID) {
-		return maximumIterations().get(loopID) == threshold();
+		return iterationsRecord().get(loopID).contains(threshold());
 	}
 
-	private boolean recordNewIteration() {
-		recordedIterations += 1;
-		return ! thresholdReached();
+	private Map<Integer, List<Integer>> iterationsRecord() {
+		return loopIterationsRecord;
 	}
 	
-	private boolean thresholdReached() {
-		return recordedIterations() == threshold();
-	}
-
-	private void resetRecordedIterations() {
-		recordedIterations = 0;
-	}
-	
-	private Map<Integer, Integer> maximumIterations() {
-		return maximumIterations;
-	}
-
-	private int recordedIterations() {
-		return recordedIterations;
-	}
-	
-	private int threshold() {
+	public int threshold() {
 		return threshold;
 	}
 	
+	public void setThreshold(Number number) {
+		threshold = number.intValue();
+	}
+	
+	public void resetThreshold() {
+		setThreshold(1E6);
+	}
+	
 	private int threshold;
-	private int recordedIterations;
-	private Map<Integer, Integer> maximumIterations;
+	private Map<Integer, List<Integer>> loopIterationsRecord;
 	
 	private static IterationsAuditor instance;
 }
