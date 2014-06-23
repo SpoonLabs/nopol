57f0999a2bc63574a559dcf68207f8e5bff48ad0
Maxime Clement <maxime.clement@inria.fr>
2014-06-23 14:02:17 +0200

    Modify the script to use GZoltar 0.0.7

--- nopol/nopol.sh
+++ nopol/nopol.sh
@@ -3,7 +3,7 @@
 DIR="`dirname $0`"
 LIB="$3"
 
-CLASSPATH=$DIR/lib/com.gzoltar-0.0.5-jar-with-dependencies.jar
+CLASSPATH=$DIR/lib/com.gzoltar-0.0.7-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$DIR/src/main/resources
 CLASSPATH=$CLASSPATH:$DIR/target/nopol-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 CLASSPATH=$CLASSPATH:$LIB
