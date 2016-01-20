c78a0c9b8ade6682efea56fa6e1483e20396eb3a
Martin Monperrus <martin.monperrus@gnieh.org>
2016-01-20 10:55:26 +0100

    travis now runs all tests

--- .travis_run_tests.sh
+++ .travis_run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-cd "nopol"
-java -cp target/nopol-0.0.3-SNAPSHOT-jar-with-dependencies.jar:target/test-classes/:misc/nopol-example/junit-4.11.jar org.junit.runner.JUnitCore fr.inria.lille.TravisTestSuite
+# test script for Travis
+cd nopol
+mvn clean test
