476c9c9b5a50c71968e0570ff050f752a6a06d2d
bdanglot <bdanglot@gmail.com>
2016-09-22 10:20:25 +0200

    dispatch exception to clients

--- nopol/src/main/java/fr/inria/lille/repair/actor/InternalNopolActor.java
+++ nopol/src/main/java/fr/inria/lille/repair/actor/InternalNopolActor.java
@@ -4,7 +4,9 @@ import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import fr.inria.lille.repair.common.config.Config;
 import fr.inria.lille.repair.common.patch.Patch;
+import fr.inria.lille.repair.nopol.NoFailingTestCaseException;
 import fr.inria.lille.repair.nopol.NoPolLauncher;
+import fr.inria.lille.repair.nopol.NoSuspiciousStatementException;
 
 import java.util.Collections;
 import java.util.List;
@@ -28,6 +30,20 @@ public class InternalNopolActor extends UntypedActor {
 			List<Patch> patches = Collections.EMPTY_LIST;
 			try {
 				patches = NoPolLauncher.launch(config.buildSourceFiles(), config.buildClasspath(), config);
+
+				//Internal debug
+				if (patches.isEmpty())
+					System.out.println("No Patch Found!");
+				else
+					for (Patch patch : patches)
+						System.out.println(patch);
+
+			} catch (NoSuspiciousStatementException | NoFailingTestCaseException noFix) {
+
+				//internal debug
+				System.out.println(noFix.toString());
+
+				client.tell(noFix, ActorRef.noSender());
 			} catch (Exception e) {
 				throw new RuntimeException("Error launch NoPol", e);
 			} finally {
@@ -35,14 +51,6 @@ public class InternalNopolActor extends UntypedActor {
 			}
 
 			client.tell(patches, ActorRef.noSender());
-
-
-			//Internal debug
-			if (patches.isEmpty())
-				System.out.println("No Patch Found!");
-			else
-				for (Patch patch : patches)
-					System.out.println(patch);
 		} else
 			unhandled(message);//Unsupported message type
 
