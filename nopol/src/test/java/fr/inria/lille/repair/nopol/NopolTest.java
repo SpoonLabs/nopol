3e6a84d75baaca28bd6f4e84e3b5c2a0923dea16
bdanglot <bdanglot@gmail.com>
2016-10-04 10:19:06 +0200

    update generated patch in test

--- nopol/src/test/java/fr/inria/lille/repair/nopol/NopolTest.java
+++ nopol/src/test/java/fr/inria/lille/repair/nopol/NopolTest.java
@@ -91,7 +91,8 @@ public class NopolTest extends TestUtility {
 				"(intermediaire < 1) && ((2) != (a))",
 				"(intermediaire == 0) && (3 <= a)",
 				"((intermediaire) - (a) + 1 < -1) && (intermediaire == 0)",
-				"!((-1 <= (1) - (a + intermediaire)) || ((0) != (intermediaire)))");
+				"!((-1 <= (1) - (a + intermediaire)) || ((0) != (intermediaire)))",
+				"(1 < a + -1) && ((a + -1 <= -1) || (intermediaire == 0))");
 	}
 
 	@Test
