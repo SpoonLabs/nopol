eba6682032be387626b57a96a491dc134b8fa762
bdanglot <bdanglot@gmail.com>
2016-10-06 15:01:06 +0200

    remove all checks to collects more constants

--- nopol/src/main/java/fr/inria/lille/spirals/repair/synthesizer/collect/spoon/DefaultConstantCollector.java
+++ nopol/src/main/java/fr/inria/lille/spirals/repair/synthesizer/collect/spoon/DefaultConstantCollector.java
@@ -1,12 +1,7 @@
 package fr.inria.lille.spirals.repair.synthesizer.collect.spoon;
 
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtLiteral;
-import spoon.reflect.code.CtLocalVariable;
-import spoon.reflect.code.CtThrow;
-import spoon.reflect.declaration.CtField;
-import spoon.reflect.declaration.CtMethod;
 
 import java.util.Map;
 
@@ -21,38 +16,14 @@ public class DefaultConstantCollector extends AbstractProcessor<CtLiteral> {
 	public DefaultConstantCollector(Map<String, Number> constants) {
 		this.constants = constants;
 		//default constants
-		constants.put("-1", -1);
-		constants.put("0", 0);
-		constants.put("1", 1);
+		this.constants.put("-1", -1);
+		this.constants.put("0", 0);
+		this.constants.put("1", 1);
 	}
 
 	@Override
 	public boolean isToBeProcessed(CtLiteral candidate) {
-		CtMethod parent = candidate.getParent(CtMethod.class);
-		if (parent == null) {
-			return false;
-		}
-
 		if (candidate.getValue() instanceof Boolean) {
-			return false;
-		} else if (candidate.getValue() instanceof Number) {// 3 defaults value will be added anyway
-			if (candidate.getValue().equals(1) ||
-					candidate.getValue().equals(0) ||
-					candidate.getValue().equals(-1)) {
-				return false;
-			}
-		}
-
-		if (candidate.getParent(CtLocalVariable.class) != null) {
-			return false;
-		}
-		if (candidate.getParent(CtAssignment.class) != null) {
-			return false;
-		}
-		if (candidate.getParent(CtField.class) != null) {
-			return false;
-		}
-		if (candidate.getParent(CtThrow.class) != null) {
 			return false;
 		}
 
