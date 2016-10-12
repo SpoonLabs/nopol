22f27cb21f46bbca36c86529ee9e1eccea009f36
bdanglot <bdanglot@gmail.com>
2016-10-12 13:36:12 +0200

    nopol example 6 is now in junit3

--- test-projects/src/test/java/nopol_examples/nopol_example_6/NopolExampleTest.java
+++ test-projects/src/test/java/nopol_examples/nopol_example_6/NopolExampleTest.java
@@ -1,47 +1,39 @@
 package nopol_examples.nopol_example_6;
 
-import static org.junit.Assert.assertEquals;
+import junit.framework.TestCase;
 
-import org.junit.Test;
-
-public class NopolExampleTest {
+public class NopolExampleTest extends TestCase {
 	
-	@Test
 	public void test1()
 	{
 		NopolExample program = new NopolExample();
 		assertEquals(program.abs(3, 5), 2);
 	}
 	
-	@Test
 	public void test2()
 	{
 		NopolExample program = new NopolExample();
 		assertEquals(program.abs(5, 3), 2);
 	}
 	
-	@Test
 	public void test3()
 	{
 		NopolExample program = new NopolExample();
 		assertEquals(program.abs(4, 0), 4);
 	}
 	
-	@Test
 	public void test4()
 	{
 		NopolExample program = new NopolExample();
 		assertEquals(program.abs(0, 4), 4);
 	}
 	
-	@Test
 	public void test5()
 	{
 		NopolExample program = new NopolExample();
 		assertEquals(program.abs(1, 1), 0);
 	}
 	
-	@Test
 	public void test6()
 	{
 		NopolExample program = new NopolExample();
