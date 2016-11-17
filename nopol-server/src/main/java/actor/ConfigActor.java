a9924e0aeee33e7a9ebfdc505f0669e4bb409cb5
bdanglot <bdanglot@gmail.com>
2016-11-17 13:59:51 +0100

    remove shaded jar

--- nopol-server/src/main/java/actor/ConfigActor.java
+++ nopol-server/src/main/java/actor/ConfigActor.java
@@ -3,14 +3,16 @@ package actor;
 import akka.actor.ActorRef;
 import fr.inria.lille.repair.common.config.Config;
 
+import java.io.Serializable;
+
 /**
  * Created by bdanglot on 11/17/16.
  * Immutable object that encapsulate the configuration of the client project to be fix.
  */
-public interface ConfigActor {
+public interface ConfigActor extends Serializable {
 
     Config getConfig();
     byte[] getContent();
-    public ActorRef getClient();
+    ActorRef getClient();
 
 }
