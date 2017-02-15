78714665d720d9b45f7736588f27209d2f7e4cea
Simon Urli <simon.urli@inria.fr>
2017-02-15 16:29:36 +0100

    Move junit library and remove hamcrest dependency. Change accordingly tests.

--- nopol/src/test/java/fr/inria/lille/repair/synthesis/DynamothCodeGenesisTest.java
+++ nopol/src/test/java/fr/inria/lille/repair/synthesis/DynamothCodeGenesisTest.java
@@ -210,7 +210,7 @@ public class DynamothCodeGenesisTest {
             tests.add(testName + "#" + next);
         }
 
-        String classpath = "../test-projects/target/test-classes"+File.pathSeparatorChar+"../test-projects/target/classes/"+File.pathSeparatorChar+"misc/nopol-example/junit-4.11.jar"+File.pathSeparatorChar+"misc/nopol-example/hamcrest-core-1.3.jar";
+        String classpath = "../test-projects/target/test-classes"+File.pathSeparatorChar+"../test-projects/target/classes/"+File.pathSeparatorChar+"lib/junit-4.11.jar";
         SourceLocation location = new SourceLocation(className, line);
         File[] files = new File []{new File("../test-projects/src/main/java/"), new File("../test-projects/src/test/java/")};
         DynamothCodeGenesis dynamothCodeGenesis = new DynamothCodeGenesisImpl(files, location, JavaLibrary.classpathFrom(classpath), oracle, tests.toArray(new String[0]),5 /* seconds, a goood value for tests */, config);
