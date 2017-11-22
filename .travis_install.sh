fdf76c5105595462885ab3f510ef474dad107d12
tdurieux <durieuxthomas@hotmail.com>
2017-11-22 10:50:11 +0100

    remove useless cocospoon installation

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,11 +4,6 @@ cd "test-projects"
 mvn clean package -DskipTests
 
 cd ..
-git clone https://github.com/SpoonLabs/CoCoSpoon.git
-cd CoCoSpoon
-mvn clean install
-
-cd ..
 # compile Nopol
 cd "nopol"
 mvn clean package -DskipTests
