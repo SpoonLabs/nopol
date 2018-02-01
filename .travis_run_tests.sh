6898b059b02fcf31110e0d649a125c6ab433426c
Martin Monperrus <martin.monperrus@gnieh.org>
2018-02-01 20:34:02 +0100

    fix broken build

--- .travis_run_tests.sh
+++ .travis_run_tests.sh
@@ -4,25 +4,17 @@
 echo "Running test suite... "
 cd nopol
 mvn clean install jacoco:report coveralls:report
+cd ..
 if [[ $? != 0 ]]
 then
     exit 1
 fi
 
-cd ..
-
 echo ${JAVA_HOME}
 
-if [[ ${JAVA_HOME} == *"7"* ]]
+# Nopol server
+if [[ ${JAVA_HOME} == *"8"* ]]
 then
-    echo "Running defects4j example... "
-    chmod +x ./.travis_run_defects4j.sh
-    ./.travis_run_defects4j.sh
-    if [[ $? != 0 ]]
-    then
-        exit 1
-    fi
-else
     echo "Running nopol server"
     cd nopol-server
     mvn clean install
@@ -30,13 +22,28 @@ else
     then
         exit 1
     fi
-
     cd ..
+fi
 
+# IDE plugin
+if [[ ${JAVA_HOME} == *"8"* ]]
+then
     echo "Running nopol-ui-intellij"
     cd nopol-ui-intellij
-
     ./gradlew buildPlugin
+    if [[ $? != 0 ]]
+    then
+        exit 1
+    fi
+    cd ..
+fi
+
+## executing one defects4J example on Jdk7
+if [[ ${JAVA_HOME} == *"7"* ]]
+then
+    echo "Running defects4j example... "
+    chmod +x ./.travis_run_defects4j.sh
+    ./.travis_run_defects4j.sh
     if [[ $? != 0 ]]
     then
         exit 1
