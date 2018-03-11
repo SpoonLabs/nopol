cede74c7247ab1b6322f32a2665c7974120d6089
Martin Monperrus <martin.monperrus@gnieh.org>
2018-03-11 18:18:25 +0100

    chore: fix deployment

--- .travis_core.sh
+++ .travis_core.sh
@@ -6,7 +6,7 @@ cd test-projects
 mvn clean package -DskipTests
 cd ..
 cd nopol
-mvn clean install jacoco:report coveralls:report
+mvn clean test
 if [[ $? != 0 ]]
 then
     exit 1
@@ -14,7 +14,7 @@ fi
 cd ..
 
 # automatic deployment to tdurieux's repo
-if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_JDK_VERSION == "openjdk8" ]; then
+if [[ $TRAVIS_BRANCH == "master" ]]; then
 	cd nopol;
         mvn versions:set -DnewVersion=`git rev-parse HEAD`
 	mvn deploy --settings ../.travis-settings.xml -DskipTests=true -B;
