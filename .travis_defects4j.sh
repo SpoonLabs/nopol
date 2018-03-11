bb1969905a98ad8f72c834cca9fa25cbfdb057be
Martin Monperrus <martin.monperrus@gnieh.org>
2018-03-11 18:15:45 +0100

    add 3 Defects4J Lang bugs for regression

--- .travis_defects4j.sh
+++ .travis_defects4j.sh
@@ -1,34 +1,11 @@
 #!/usr/bin/env bash
-
-project=Chart
-id=5
-wd=${project}_${id}
-
+# executing all tests in Defects4jEvaluationTest
 cd nopol
-mvn clean install -DskipTests
+mvn -q versions:set -DnewVersion=TRAVIS
+# creating target/nopol-TRAVIS-jar-with-dependencies.jar
+mvn -q clean package -DskipTests
 
-git clone https://github.com/rjust/defects4j.git
-cd defects4j
-./init.sh
-cd ../
-defects4j/framework/bin/defects4j checkout -p ${project} -v ${id}b -w ${wd}
-cd ${wd}
-../defects4j/framework/bin/defects4j compile
-../defects4j/framework/bin/defects4j test | tee > out-defects4j-test.txt
+env NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="Defects4jEvaluationTest"
 
-fullQualifiedNameFailingTestCase=$(cat out-defects4j-test.txt | grep - | cut -d ":" -f1 | cut -d "-" -f2)
-echo ${fullQualifiedNameFailingTestCase}
 
-cd ../
-echo "java -cp ${JAVA_HOME}/lib/tools.jar:target/nopol-0.2-SNAPSHOT-jar-with-dependencies.jar fr.inria.lille.repair.Main -s ${wd}/source/ -c ${wd}/build/:${wd}/build-tests:${wd}/lib/:defects4j/framework/projects/lib/cobertura-lib/servlet-api-2.5-6.1.14.jar -p lib/z3/z3_for_linux -t ${fullQualifiedNameFailingTestCase} -z gzoltar | tee out.txt"
-java -cp ${JAVA_HOME}/lib/tools.jar:target/nopol-0.2-SNAPSHOT-jar-with-dependencies.jar fr.inria.lille.repair.Main -s ${wd}/source/ -c ${wd}/build/:${wd}/build-tests:${wd}/lib/:defects4j/framework/projects/lib/cobertura-lib/servlet-api-2.5-6.1.14.jar -p lib/z3/z3_for_linux -t ${fullQualifiedNameFailingTestCase} -z gzoltar | tee out.txt
-if [[ $? != 0 ]]
-then
-    exit 1
-fi
-# A patch has been found, compare to it the known patch.
-cat out.txt | grep -A1 "PATCH FOUND" | grep -e "!(org.jfree.data.xy.XYSeries.this.allowDuplicateXValues)" -e "overwritten!=null"
-if [[ $? != 0 ]]
-then
-    exit 1
-fi
+
---
+++ nopol/src/test/java/fr/inria/lille/repair/nopol/Defects4jEvaluationTest.java
@@ -0,0 +1,111 @@
+package fr.inria.lille.repair.nopol;
+
+import fr.inria.lille.commons.synthesis.smt.solver.SolverFactory;
+import fr.inria.lille.repair.TestUtility;
+import fr.inria.lille.repair.common.config.NopolContext;
+import fr.inria.lille.repair.common.synth.RepairType;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+public class Defects4jEvaluationTest {
+
+	private final static int TIMEOUT=5*60*1000;// 5 minutes in millisecs
+
+	public boolean testShouldBeRun() {
+		if (System.getenv("NOPOL_EVAL_DEFECTS4J")==null) {
+			return false;
+		}
+		return true;
+	}
+
+	public NopolContext nopolConfigFor(String bug_id) throws Exception {
+		String folder = "unknown";
+		if (!new File(bug_id).exists()) {
+			new File (bug_id).mkdir();
+			Runtime.getRuntime().exec("git init "+bug_id);
+			String command = "cd " + bug_id + "; git fetch https://github.com/Spirals-Team/defects4j-repair " + bug_id + ":" + bug_id + "; git checkout "+bug_id+";mvn -q test -DskipTests; mvn dependency:build-classpath -Dmdep.outputFile=cp.txt";
+			System.out.println(command);
+			Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", command});
+			p.waitFor();
+			String output = IOUtils.toString(p.getInputStream());
+			String errorOutput = IOUtils.toString(p.getErrorStream());
+			System.out.println(output);
+			//System.err.println(errorOutput);
+		}
+
+		Properties prop = new Properties();
+		prop.load(new FileInputStream(bug_id+"/defects4j.build.properties"));
+
+		NopolContext nopolContext = new NopolContext();
+		String src = bug_id+"/"+prop.get("d4j.dir.src.classes");
+		nopolContext.setProjectSourcePath(new File[]{new File(src)});
+
+		// getting the classpath from Maven
+		List<URL> cp = new ArrayList<>();
+		for (String entry : FileUtils.readFileToString(new File(bug_id+"/cp.txt")).split(new String(new char[]{File.pathSeparatorChar}))) {
+			cp.add(new File(entry).toURL());
+		}
+		cp.add(new File(bug_id+"/target/classes").toURL());
+		cp.add(new File(bug_id+"/target/test-classes").toURL());
+		System.out.println(cp);
+//
+		nopolContext.setProjectClasspath(cp.toArray(new URL[0]));
+
+		nopolContext.setType(RepairType.PRE_THEN_COND);
+
+		SolverFactory.setSolver("z3", TestUtility.solverPath);
+
+		return nopolContext;
+	}
+
+	@Test(timeout = TIMEOUT)
+	public void test_Lang44() throws Exception {
+		if (!testShouldBeRun()) { return; }
+		NopolContext nopolContext = nopolConfigFor("Lang44");
+		nopolContext.setComplianceLevel(4);
+		NopolResult result = new NoPol(nopolContext).build();
+		assertEquals(1, result.getPatches().size());
+	}
+
+	@Test(timeout = TIMEOUT)
+	public void test_Lang51() throws Exception {
+		if (!testShouldBeRun()) { return; }
+		NopolContext nopolContext = nopolConfigFor("Lang51");
+		nopolContext.setComplianceLevel(4);
+		NopolResult result = new NoPol(nopolContext).build();
+		assertEquals(1, result.getPatches().size());
+	}
+
+	@Test(timeout = TIMEOUT)
+	public void test_Lang53() throws Exception {
+		if (!testShouldBeRun()) { return; }
+		NopolContext nopolContext = nopolConfigFor("Lang53");
+		nopolContext.setComplianceLevel(4);
+		NopolResult result = new NoPol(nopolContext).build();
+		assertEquals(1, result.getPatches().size());
+	}
+
+//	@Test(timeout = TIMEOUT)
+//	public void test_Lang58() throws Exception {
+//		//if (!testShouldBeRun()) { return; }
+//		NopolContext nopolContext = nopolConfigFor("Lang58");
+//		// problem: mvn test does not work, the pom.xml of Defects4j does not setup java version with 1.4 at the beginning
+//		nopolContext.setComplianceLevel(4);
+//		NopolResult result = new NoPol(nopolContext).build();
+//		assertEquals(1, result.getPatches().size());
+//	}
+
+}
+
+
+
