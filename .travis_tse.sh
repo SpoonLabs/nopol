e6ab27f20e87810685a90256028fb04f0cafa960
Martin Monperrus <martin.monperrus@gnieh.org>
2018-03-11 11:19:24 +0100

    fix TSE eval Travis

--- .travis_tse.sh
+++ .travis_tse.sh
@@ -24,6 +24,8 @@ echo "# does nothing, only compile" >> call_nopol.sh
 chmod 755 call_nopol.sh
 
 # only compiling
+
+# commons math
 python src/reproduce.py -bug cm1  || true
 python src/reproduce.py -bug cm2  || true
 python src/reproduce.py -bug cm3  || true
@@ -32,7 +34,8 @@ python src/reproduce.py -bug cm5  || true
 python src/reproduce.py -bug cm6  || true
 python src/reproduce.py -bug cm7  || true
 python src/reproduce.py -bug cm10  || true
-# python src/reproduce.py -bug cm5
+python src/reproduce.py -bug pm1  || true
+python src/reproduce.py -bug pm2  || true
 
 # Commons lang
 python src/reproduce.py -bug cl1  || true
@@ -41,7 +44,6 @@ python src/reproduce.py -bug cl3  || true
 python src/reproduce.py -bug cl4  || true
 python src/reproduce.py -bug cl5  || true
 python src/reproduce.py -bug cl6  || true
-
 python src/reproduce.py -bug pl1  || true
 python src/reproduce.py -bug pl2  || true
 python src/reproduce.py -bug pl3  || true
