e5cbf0570e7f6d6ef2631dc415ca9f0adf8faa09
Martin Monperrus <martin.monperrus@gnieh.org>
2018-09-28 21:22:31 +0200

    up

--- nopol/src/test/java/fr/inria/lille/repair/nopol/Defects4jUtils.java
+++ nopol/src/test/java/fr/inria/lille/repair/nopol/Defects4jUtils.java
@@ -51,7 +51,7 @@ public class Defects4jUtils {
 			String command = "mkdir " + bug_id +";\n cd " + bug_id + ";\n git init;\n git fetch https://github.com/Spirals-Team/defects4j-repair " + bug_id + ":" + bug_id + ";\n git checkout "+bug_id+";\n"
 			+"sed '/delete dir/ d' ant/build.xml;\n"
 			+"ant -f ant/build.xml compile compile-tests;\n"
-			+"echo -n `pwd`/lib/junit.jar:`pwd`/lib/iText-2.1.4.jar:`pwd`/lib/junit.jar:`pwd`/lib/servlet.jar > cp.txt;\n"
+			+"echo -n `pwd`/lib/iText-2.1.4.jar:`pwd`/lib/junit.jar:`pwd`/lib/servlet.jar > cp.txt;\n"
 
 
 					;
