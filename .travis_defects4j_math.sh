5b09188369d1fdfe34c6eb5a86558a0b087e4c56
Martin Monperrus <martin.monperrus@gnieh.org>
2018-09-29 19:34:16 +0200

    up

--- .travis_defects4j_math.sh
+++ .travis_defects4j_math.sh
@@ -6,13 +6,13 @@ set -e
 
 cd nopol
 
-NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math42"
+NOPOL_EVAL_DEFECTS4J=1 mvn test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math42"
 
-NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math49"
+NOPOL_EVAL_DEFECTS4J=1 mvn test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math49"
 
-NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math69"
+NOPOL_EVAL_DEFECTS4J=1 mvn test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math69"
 
-# NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math32"
+# NOPOL_EVAL_DEFECTS4J=1 mvn test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math32"
 
-NOPOL_EVAL_DEFECTS4J=1 mvn -q test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math33"
+NOPOL_EVAL_DEFECTS4J=1 mvn test -Dtest="fr.inria.lille.repair.nopol.Defects4jEvaluationMathTest#test_Math33"
 
