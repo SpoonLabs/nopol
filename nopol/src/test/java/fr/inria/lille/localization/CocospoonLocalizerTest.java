d8a610ed3d38d31261e35b3cecc4927f2c95a1e1
DehengYang <1660542154@qq.com>
2019-02-01 11:56:20 +0800

    Update CocospoonLocalizerTest to expose the cocospoon problem
    This test aims to expose the java.lang.RuntimeException when running test cases in Math_58, a real bug from defects4j. But this test file (CocospoonLocalizerTest.java) is created based on the version of Nopol of March 2017.

--- nopol/src/test/java/fr/inria/lille/localization/CocospoonLocalizerTest.java
+++ nopol/src/test/java/fr/inria/lille/localization/CocospoonLocalizerTest.java
@@ -11,93 +11,38 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
+import java.util.*; //added by deheng
+
 import static gov.nasa.jpf.util.test.TestJPF.assertEquals;
 import static gov.nasa.jpf.util.test.TestJPF.assertTrue;
 
 /**
- * Created by bdanglot on 10/4/16.
+ * Modified by deheng on Feb 1, 2019.
+ * This test aims to expose the java.lang.RuntimeException when running test cases in Math_58, a real bug from defects4j. But this test file (CocospoonLocalizerTest.java) is created based on the version of Nopol of March 2017.
+ * Use `mvn clean package -DskipTests` to build nopol and use `mvn test -Dtest=CocospoonLocalizerTest` to run this test.
  */
 public class CocospoonLocalizerTest {
 
     @Test
     public void testOchiaiCoCoSpoonLocalizer() throws Exception {
-
-		/* test OchiaiCoCoSpoonLocalizer : the SourceLocation must be sorted following the Ochiai metric */
-
-        File[] sources = new File[]{new File("../test-projects/src/main/java/nopol_examples/nopol_example_1/NopolExample.java")};
+        File[] sources = new File[]{new File("/home/apr/environment/buggyPrograms/NopolRepairPrograms/Math/Math_58/src/main/java")}; // This is the source file of Math_58 
+	String tests="org.apache.commons.math.distribution.HypergeometricDistributionTest"; // This test is the targeted test that can expose the problem of cocospoon.
         URL[] classpath = new URL[]{
-                new File("../test-projects/target/classes").toURI().toURL(),
-                new File("../test-projects/target/test-classes").toURI().toURL()
-        };
-        String[] testClasses = new String[]{"nopol_examples.nopol_example_1.NopolExampleTest"};
+                new File("/home/apr/environment/buggyPrograms/NopolRepairPrograms/Math/Math_58/target/classes/").toURI().toURL(),
+                new File("/home/apr/environment/buggyPrograms/NopolRepairPrograms/Math/Math_58/target/test-classes/").toURI().toURL()
+        }; // This corresponds to the paths of java classes files.
+	String[] testClasses = tests.split(", ");
         CocoSpoonBasedSpectrumBasedFaultLocalizer localizer = new CocoSpoonBasedSpectrumBasedFaultLocalizer(new NopolContext(sources, classpath, testClasses), new Ochiai());
         Map<SourceLocation, List<TestResult>> executedSourceLocationPerTest = localizer.getTestListPerStatement();
-        assertEquals(8, executedSourceLocationPerTest.keySet().size());
 
-        SourceLocation sourceLocation1 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 18);
-        SourceLocation sourceLocation2 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 13);
-        SourceLocation sourceLocation3 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 12);
-        SourceLocation sourceLocation4 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 15);
-        SourceLocation sourceLocation5 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 16);
-        //CoCoSpoon is able to log in constructor: 3 SourceLocation more than GZoltar
-        SourceLocation sourceLocation6 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 22);
-        SourceLocation sourceLocation7 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 23);
-        SourceLocation sourceLocation8 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 24);
+        SourceLocation sourceLocation1 = new SourceLocation("org.apache.commons.math.optimization.general.LevenbergMarquardtOptimizer", 741);
 
+	//System.out.println("dale stmt 0:"+executedSourceLocationPerTest.size()+"   "+new ArrayList<>(executedSourceLocationPerTest.keySet()).get(0) +new ArrayList<>(executedSourceLocationPerTest.keySet()).get(0).getSuspiciousValue()  );
         assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation1));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation2));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation3));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation4));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation5));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation6));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation7));
-        assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation8));
 
-        List<? extends StatementSourceLocation> sortedStatements = localizer.getStatements();
+        List<SourceLocation> sortedStatements = new ArrayList<>(executedSourceLocationPerTest.keySet());
 
-        assertEquals(0.534, sortedStatements.get(0).getSuspiciousness(), 10E-3);
-        assertEquals(0.5, sortedStatements.get(1).getSuspiciousness(), 10E-3);
-        assertEquals(0.471, sortedStatements.get(2).getSuspiciousness(), 10E-3);
-        assertEquals(0.471, sortedStatements.get(3).getSuspiciousness(), 10E-3);
-        assertEquals(0.471, sortedStatements.get(4).getSuspiciousness(), 10E-3);
-        assertEquals(0.471, sortedStatements.get(5).getSuspiciousness(), 10E-3);
-        assertEquals(0.0, sortedStatements.get(6).getSuspiciousness(), 10E-3);
-        assertEquals(0.0, sortedStatements.get(7).getSuspiciousness(), 10E-3);
-
-        assertEquals(sourceLocation5, sortedStatements.get(0).getLocation());
-    }
-
-    @Test
-    public void test2() throws Exception {
-
-        // specifc tests (eg #test2) works
-
-        File[] sources = new File[]{new File("../test-projects/src/main/java/nopol_examples/nopol_example_1/NopolExample.java")};
-        URL[] classpath = new URL[]{
-                new File("../test-projects/target/classes").toURI().toURL(),
-                new File("../test-projects/target/test-classes").toURI().toURL()
-        };
-        String[] testClasses = new String[]{"nopol_examples.nopol_example_1.NopolExampleTest#test1"};
-
-        NopolContext nopolContext = new NopolContext(sources, classpath, testClasses);
-        FaultLocalizer localizer = new CocoSpoonBasedSpectrumBasedFaultLocalizer(nopolContext);
-        Map<SourceLocation, List<TestResult>> executedSourceLocationPerTest = localizer.getTestListPerStatement();
-        Assert.assertEquals(5, executedSourceLocationPerTest.keySet().size());
-
-        SourceLocation sourceLocation2 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 13);
-        SourceLocation sourceLocation3 = new SourceLocation("nopol_examples.nopol_example_1.NopolExample", 12);
-
-        for (SourceLocation loc : executedSourceLocationPerTest.keySet()) {
-            for (TestResult res : executedSourceLocationPerTest.get(loc)) {
-                Assert.assertEquals("nopol_examples.nopol_example_1.NopolExampleTest#test1", res.getTestCase().toString());
-            }
-        }
-
-        System.out.println(executedSourceLocationPerTest);
-        Assert.assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation2));
-        Assert.assertTrue(executedSourceLocationPerTest.keySet().contains(sourceLocation3));
-
-
+        assertEquals(1, sortedStatements.get(0).getSuspiciousValue(), 10E-3);
     }
 
 }
