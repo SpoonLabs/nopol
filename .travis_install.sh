3242c2154caa56bf9d7322d1bbe6bf1101220eb1
Martin Monperrus <martin.monperrus@gnieh.org>
2019-02-17 16:33:26 +0100

    up

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,6 +6,7 @@ mvn clean package -DskipTests
 cd ..
 git clone https://github.com/SpoonLabs/CoCoSpoon.git
 cd CoCoSpoon
+git checkout -b 975b9b07e04fc8a689f4b4ca2746ccb49b4c380e 975b9b07e04fc8a689f4b4ca2746ccb49b4c380e
 mvn clean install
 
 cd ..
