aab1e885444cb2fe644574b28e27854bab4e5afb
DehengYang <1660542154@qq.com>
2019-02-18 09:33:18 +0800

    Update .travis_install.sh

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,9 +4,13 @@ cd "test-projects"
 mvn clean package -DskipTests
 
 cd ..
-git clone -b compat-java7 https://github.com/SpoonLabs/CoCoSpoon.git
+# git clone -b compat-java7 https://github.com/SpoonLabs/CoCoSpoon.git
+# cd CoCoSpoon
+# mvn clean install -DskipTests
+git clone https://github.com/SpoonLabs/CoCoSpoon.git
 cd CoCoSpoon
-mvn clean install -DskipTests
+git checkout -b 975b9b07e04fc8a689f4b4ca2746ccb49b4c380e 975b9b07e04fc8a689f4b4ca2746ccb49b4c380e
+mvn clean install
 
 cd ..
 # compile Nopol
